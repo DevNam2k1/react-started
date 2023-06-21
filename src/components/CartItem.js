2a657de8066e13b204b666efb76fbd92d61b732b
Jade Nguyen <jade.nguyen@savvycomsoftware.com>
2023-06-21 17:45:56 +0700

    ✨Feat: Study react  building  ecomerce shop

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+import { Link } from 'react-router-dom';
+
+import { IoMdClose } from 'react-icons/io';
+
+const CartItem = ({item}) => {
+  const { id, image, category, title, price } = item;
+
+  return (
+    <div className='flex'>
+      <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+        <div>
+          <Link to={`/product/${id}`}>
+          <img className='max-w-[80px]' src={image} alt='' />
+          </Link>
+        </div>
+        <div>
+          <Link to={`/product/${id}`} className='text-sm uppercase 
+          font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
